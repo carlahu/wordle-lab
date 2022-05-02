ceaad4ef84373926386b636932709710e38bfcca
Carla <55294047+carlahu@users.noreply.github.com>
2022-05-02 11:33:37 -0400

    Update wordle.java

--- wordle.java
+++ wordle.java
@@ -5,59 +5,46 @@ import java.util.Scanner;
 import java.util.Random;
 
 public class wordle{
-  static Scanner input;
-  final String word = blob;
-  
-  public String userGuess(int guessNumber){
-    Scanner input1 = new Scanner(System.in);
-        System.out.println("What is your guess?");
-        String myAnswer = input1.nextLine();
-        return myAnswer;
-  }
-  
-  public Character[] convertAnswer(String s){
-    if (s == null) {
-        return null;
-    }
-    Character[] array = new Character[s.length()];
-    for (int i = 0; i < s.length(); i++) {
-        array[i] = new Character(s.charAt(i));
-    }
-    return array;
-  }
+  static Scanner input = new Scanner(System.in);
+  final static String word_of_day = "blob";
+  static String[] solution = new String[] {"_","_","_","_"};
+  static final String[] ALPHABET = new String[]{"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"};
+  static final String[] user_ALPHABET = new String[]{"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"};
   
   
   public static void main(String [] args){
-    List<String> arr = new ArrayList<>();
-    arr.add("A");
-    arr.add("B");
-    arr.add("C");
-    arr.add("D");
-    arr.add("E");
-    arr.add("F");
-    arr.add("G");
-    arr.add("H");
-    arr.add("I");
-    arr.add("J");
-    arr.add("K");
-    arr.add("L");
-    arr.add("M");
-    arr.add("N");
-    arr.add("O");
-    arr.add("P");
-    arr.add("Q");
-    arr.add("R");
-    arr.add("S");
-    arr.add("T");
-    arr.add("U");
-    arr.add("V");
-    arr.add("W");
-    arr.add("X");
-    arr.add("Y");
-    arr.add("Z");
-    
+    int guessCount = 0;
+    while(guessCount < 6){
+      guessCount++;
+      String userGuess = guess(guessCount);
+      char[] guessAsChar = guessToChar(userGuess);
+      evaluateGuess(guessAsChar(userGuess));
+      printInfo(solution,user_ALPHABET);
+      System.out.println("--------------------");
+      
+      if(checkSolution()){
+       break; 
+      }
+    }if(checkSolution()){
+     System.out.println("Goodjob!"); 
+    }else{
+      System.out.println("Oh, no, you ran out of guesses");
+    }
+  }
+  public static String guess(int guess){
+    System.out.println("Guess Number " + guess);
+    String userInput = Input.nextLine();
+    while(userInput.length() != 5){
+      System.out.println("Error, need 5 letter guesses.");
+      userInput = Input.nextLine();
+    }
+    if(userInput.length() == 5){
+     return guess;
+    }
     
   }
+  
+  
 }
   
     
